e3873ed8db63b29951ecfeb2362e943433014621
steirico <steirico@users.noreply.github.com>
2019-01-05 12:27:55 +0100

    Do not initialze rcrop if the field is missing

--- fields/assets/js/imagecrop.js
+++ fields/assets/js/imagecrop.js
@@ -1,68 +1,72 @@
 var initImageCrop = function(){
-    // get elements
-    var $figure = $($.find('.fileview-image')[0]);
-    var $image = $($figure.find('img')[0]);
-    var $parent = $image.parent()
-    var $wrapper = $('<div class="image-wrapper"></div>');
-    var $field = $($.find('.field-name-crop')[0]);
-    var $fieldGrid = $($field.find('.field-grid')[0]);
-    var minW = parseInt($fieldGrid.attr('data-min-w'), 10);
-    var minH = parseInt($fieldGrid.attr('data-min-h'), 10);
-    var aspectRatio = $fieldGrid.attr('data-aspect-ratio');
+    var field = $.find('.field-name-crop');
 
-    var inputs = {
-        x : $($field.find('#X')),
-        y : $($field.find('#Y')),
-        width : $($field.find('#W')),
-        height : $($field.find('#H'))
-    };
+    if (0 !== field.length) {
+        // get elements
+        var $figure = $($.find('.fileview-image')[0]);
+        var $image = $($figure.find('img')[0]);
+        var $parent = $image.parent()
+        var $wrapper = $('<div class="image-wrapper"></div>');
+        var $field = $(field[0]);
+        var $fieldGrid = $($field.find('.field-grid')[0]);
+        var minW = parseInt($fieldGrid.attr('data-min-w'), 10);
+        var minH = parseInt($fieldGrid.attr('data-min-h'), 10);
+        var aspectRatio = $fieldGrid.attr('data-aspect-ratio');
 
-    // setup wrapper
-    $image.appendTo($wrapper);
-    $parent.replaceWith($wrapper);
+        var inputs = {
+            x : $($field.find('#X')),
+            y : $($field.find('#Y')),
+            width : $($field.find('#W')),
+            height : $($field.find('#H'))
+        };
 
-    // init rcrop
-    $image.rcrop({
-        minSize : [
-            minW || 160,
-            minH || 90
-        ],
-        preserveAspectRatio : aspectRatio,
-        grid : true
-    });
+        // setup wrapper
+        $image.appendTo($wrapper);
+        $parent.replaceWith($wrapper);
 
-    function getCropValues(){
-        var values = $image.rcrop('getValues');
+        // init rcrop
+        $image.rcrop({
+            minSize : [
+                minW || 160,
+                minH || 90
+            ],
+            preserveAspectRatio : aspectRatio,
+            grid : true
+        });
+
+        function getCropValues(){
+            var values = $image.rcrop('getValues');
+            for(var key in inputs){
+                inputs[key].val(values[key]);
+            }
+        }
+
+        function setCropValues(trigger){
+            var
+                id = trigger ? trigger.target.id : "",
+                x = inputs['x'].val(),
+                y = inputs['y'].val(),
+                width = inputs['width'].val(),
+                height = inputs['height'].val(),
+                settings = $image.rcrop('instance').settings;
+
+            // TODO calc proper W and H on manual setting of new values
+            if("W" == id){
+                $image.rcrop('resize', width, false, x, y);
+            } else if ("H" == id){
+                $image.rcrop('resize', false, height, x, y);
+            } else {
+                $image.rcrop('resize', width, height, x, y);
+            }
+        }
+
         for(var key in inputs){
-            inputs[key].val(values[key]);
+            inputs[key].on('focusout', setCropValues);
         }
+
+        $image.on('rcrop-ready', setCropValues);
+        $image.on('rcrop-changed', getCropValues);
     }
-
-    function setCropValues(trigger){
-        var
-            id = trigger ? trigger.target.id : "",
-            x = inputs['x'].val(),
-            y = inputs['y'].val(),
-            width = inputs['width'].val(),
-            height = inputs['height'].val(),
-            settings = $image.rcrop('instance').settings;
-
-        // TODO calc proper W and H on manual setting of new values
-        if("W" == id){
-            $image.rcrop('resize', width, false, x, y);
-        } else if ("H" == id){
-            $image.rcrop('resize', false, height, x, y);
-        } else {
-            $image.rcrop('resize', width, height, x, y);
-        }
-    }
-
-    for(var key in inputs){
-        inputs[key].on('focusout', setCropValues);
-    }
-
-    $image.on('rcrop-ready', setCropValues);
-    $image.on('rcrop-changed', getCropValues);
 };
 
 $(document).ready(initImageCrop);
